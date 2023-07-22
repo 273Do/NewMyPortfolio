da82e0f9799ad35f43e06911bfe9a43bddad94eb
273* <keita.y310@icloud.com>
2023-07-22 16:37:13 +0900

    フッターを追加

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import myLogo from "../images/273DoLogo.png";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <header>
+      <h1 class="site_logo">
+        <a href="#">
+          <img src={myLogo} alt="273*Logo" />
+        </a>
+      </h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Main</a>
+          </li>
+          <li>
+            <a href="#">Tools</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">Design</a>
+          </li>
+          <li>
+            <a href="#">Club / Other_Activities</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
